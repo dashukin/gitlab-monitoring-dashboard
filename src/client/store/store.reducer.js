d0869fdce1645ee21e20fa2926bf02306ae7a626
dashukin <dashukin@gmail.com>
2019-09-08 16:03:06 +0300

    Add db to app state

--- src/client/store/store.reducer.js
+++ src/client/store/store.reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import page from 'src/client/store/reducers/page/page.reducer';
-import example from 'src/client/store/__example/example.reducer';
+import db from 'src/client/store/reducers/db/db.reducer';
+import example from 'src/client/store/reducers/__example/example.reducer';
 
 export const reducers = {
+  db,
   page,
   example,
 };
