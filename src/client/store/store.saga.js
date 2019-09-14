b6dad4f878f89b3154b2143c87f996ff406a8601
dashukin <dashukin@gmail.com>
2019-09-15 00:30:24 +0300

    Update root saga execution flow

--- src/client/store/store.saga.js
+++ src/client/store/store.saga.js
@@ -1,10 +1,12 @@
 /*
 * Root saga
 * */
-import { fork, all } from 'redux-saga/effects';
+import { fork, spawn, all } from 'redux-saga/effects';
 import map from 'lodash/fp/map';
 import { initI18n, watchI18n } from 'src/client/store/reducers/i18n/i18n.saga';
-import { watchExample } from 'src/client/store/__example/example.saga';
+import { watchExample } from 'src/client/store/reducers/__example/example.saga';
+import { watchProjects } from 'src/client/store/reducers/projects/projects.saga';
+import { watchMergeRequests } from 'src/client/store/reducers/merge-requests/merge-requests.saga';
 
 export const startSagas = [
   initI18n,
@@ -13,12 +15,15 @@ export const startSagas = [
 export const watchSagas = [
   watchI18n,
   watchExample,
+  watchProjects,
+  watchMergeRequests,
 ];
 
 export function* watchSaga() {
-  yield all(map(fork, watchSagas));
+  yield all(map(spawn, watchSagas));
 }
 
 export function* rootSaga() {
+  yield spawn(watchSaga);
   yield all(map(fork, startSagas));
 }
